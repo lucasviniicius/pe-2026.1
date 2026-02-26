dadfd1d7d65eb83a869af474970ee2d8230a422b
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-02-26 13:53:55 +0000

    exibe as idades acima da media

--- aula-02/pesquisa-idade.c
+++ aula-02/pesquisa-idade.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int idade, qtdPessoa = 0, soma = 0;
+    int idade, qtdPessoa = 0, soma = 0, idades[1000];
     double media;
     
     printf("Digite sua idade: ");
     scanf("%d", &idade);
 
     while(idade >= 0){
-        qtdPessoa += 1;
+        idades[qtdPessoa] = idade;
+        qtdPessoa++;
         soma += idade;
         printf("Digite sua idade: ");
         scanf("%d", &idade);
@@ -16,7 +17,14 @@ int main(){
 
     if(qtdPessoa != 0){
         media = (double) soma / qtdPessoa;
-        printf("Média: %.2lf", media);
+        printf("Média: %.2lf\n", media);
+
+        printf("Idades acima da média:\n");
+        for(int i = 0; i < qtdPessoa; i++){
+            if(idades[i] > media){
+                printf("%d\n", idades[i]);
+            }
+        }
     } else {
         printf("Idade inválida!");
     }
