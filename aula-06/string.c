bae810a692339316a0603003591dff4ae137e46d
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-03-19 22:58:43 +0000

    strings

---
+++ aula-06/string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 50
+
+void lerStr(char str[], int tam);
+int meuStrLen(char str[]);
+
+int main(){
+    char mensagem[MAX] = "Boas-vindas a turma de PE";
+    int tamanhoString = meuStrLen(mensagem);
+
+    printf("%s", mensagem);
+    lerStr(mensagem, MAX);
+
+    printf("\nTamanho da string: %d", tamanhoString);
+}
+
+void lerStr(char str[], int tamMax){
+    fgets(str, tamMax, stdin);
+
+    int tam = strlen(str);
+
+    if(tam > 0 && str[tam - 1] != '\n'){
+        str[tam - 1] = '\0';
+    }
+}
+
+int meuStrLen(char str[]){
+    int tam = 0;
+
+    while(str[tam] != '\0'){
+        tam++;
+    }
+
+    return tam;
+}
